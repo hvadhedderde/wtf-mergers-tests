e3d3b8fac1db502a4a8ba38cd5d2d8e7fc5bb0df
kaestel <martin@hvadhedderde.com>
2013-04-14 23:08:43 +0200

    test includes

--- src/js/lib/seg_desktop_include.js
+++ src/js/lib/seg_desktop_include.js
@@ -1,19 +1,3 @@
-document.write('<script type="text/javascript" src="/u.js"></script>');
-document.write('<script type="text/javascript" src="/u-debug.js"></script>');
-
-document.write('<script type="text/javascript" src="/u-animation.js"></script>');
-document.write('<script type="text/javascript" src="/u-cookie.js"></script>');
-document.write('<script type="text/javascript" src="/u-date.js"></script>');
-document.write('<script type="text/javascript" src="/u-dom.js"></script>');
-document.write('<script type="text/javascript" src="/u-events.js"></script>');
-document.write('<script type="text/javascript" src="/u-flash.js"></script>');
-document.write('<script type="text/javascript" src="/u-form.js"></script>');
-document.write('<script type="text/javascript" src="/u-hash.js"></script>');
-document.write('<script type="text/javascript" src="/u-image.js"></script>');
-document.write('<script type="text/javascript" src="/u-link.js"></script>');
-document.write('<script type="text/javascript" src="/u-position.js"></script>');
-document.write('<script type="text/javascript" src="/u-request.js"></script>');
-document.write('<script type="text/javascript" src="/u-string.js"></script>');
-document.write('<script type="text/javascript" src="/u-system.js"></script>');
-document.write('<script type="text/javascript" src="/u-timer.js"></script>');
-
+document.write('<script type="text/javascript" src="http://test.whattheframework.org/u.js"></script>');
+document.write('<script type="text/javascript" src="u-relative.js"></script>');
+document.write('<script type="text/javascript" src="/test/src/js/lib/u-absolute.js"></script>');
--- src/js/seg_mobile_light.js
+++ src/js/seg_mobile_light.js
@@ -6,88 +6,6 @@ if(!u || !Util) {
 	u.stats = new function() {this.pageView = function(){};this.event = function(){};this.customVar = function(){}}
 }
 
-Util.debugURL = function(url) {
-	if(u.bug_force) {
-		return true;
-	}
-	return document.domain.match(/.local$/);
-}
-Util.nodeId = function(node, include_path) {
-	try {
-		if(!include_path) {
-			return node.id ? node.nodeName+"#"+node.id : (node.className ? node.nodeName+"."+node.className : (node.name ? node.nodeName + "["+node.name+"]" : node.nodeName));
-		}
-		else {
-			if(node.parentNode && node.parentNode.nodeName != "HTML") {
-				return u.nodeId(node.parentNode, include_path) + "->" + u.nodeId(node);
-			}
-			else {
-				return u.nodeId(node);
-			}
-		}
-	}
-	catch(exception) {
-		u.bug("Exception ("+exception+") in u.nodeId("+node+"), called from: "+arguments.callee.caller);
-	}
-	return "Unindentifiable node!";
-}
-Util.bug = function(message, corner, color) {
-	if(u.debugURL()) {
-		if(!u.bug_console_only) {
-			var option, options = new Array([0, "auto", "auto", 0], [0, 0, "auto", "auto"], ["auto", 0, 0, "auto"], ["auto", "auto", 0, 0]);
-			if(isNaN(corner)) {
-				color = corner;
-				corner = 0;
-			}
-			if(typeof(color) != "string") {
-				color = "black";
-			}
-			option = options[corner];
-			if(!u.qs("#debug_id_"+corner)) {
-				var d_target = u.ae(document.body, "div", {"class":"debug_"+corner, "id":"debug_id_"+corner});
-				d_target.style.position = u.bug_position ? u.bug_position : "absolute";
-				d_target.style.zIndex = 16000;
-				d_target.style.top = option[0];
-				d_target.style.right = option[1];
-				d_target.style.bottom = option[2];
-				d_target.style.left = option[3];
-				d_target.style.backgroundColor = u.bug_bg ? u.bug_bg : "#ffffff";
-				d_target.style.color = "#000000";
-				d_target.style.textAlign = "left";
-				if(d_target.style.maxWidth) {
-					d_target.style.maxWidth = u.bug_max_width ? u.bug_max_width+"px" : "auto";
-				}
-				d_target.style.padding = "3px";
-			}
-			if(typeof(message) != "string") {
-				message = message.toString();
-			}
-			u.ae(u.qs("#debug_id_"+corner), "div", ({"style":"color: " + color})).innerHTML = message ? message.replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/&lt;br&gt;/g, "<br>") : "Util.bug with no message?";
-		}
-		if(typeof(console) == "object") {
-			console.log(message);
-		}
-	}
-}
-Util.xInObject = function(object) {
-	if(u.debugURL()) {
-		var x, s = "--- start object ---<br>";
-		for(x in object) {
-			if(object[x] && typeof(object[x]) == "object" && typeof(object[x].nodeName) == "string") {
-				s += x + "=" + object[x]+" -> " + u.nodeId(object[x], 1) + "<br>";
-			}
-			else if(object[x] && typeof(object[x]) == "function") {
-				s += x + "=function<br>";
-			}
-			else {
-				s += x + "=" + object[x]+"<br>";
-			}
-		}
-		s += "--- end object ---"
-		u.bug(s);
-	}
-}
-
 <html>
     <head>
         <title>  </title>
@@ -168,11 +86,11 @@ Util.xInObject = function(object) {
             }
             function bdetect() {
                 var loc = bredir(
-                    'localhostu-test.js', 
-                    'localhostu-test.js', 
+                    'localhostu-relative.js', 
+                    'localhostu-relative.js', 
                     '', 
                     'error', 
-                    '/main?wc=EWJnGhd5DB5fBBBzGAkFHQ%3D%3D&url=localhostu-test.js'
+                    '/main?wc=EWJnGhd5DB5fBBBzGAkFHQ%3D%3D&url=localhostu-relative.js'
                 );
                 if(typeof loc === 'undefined') {
                     self.close();
@@ -184,7 +102,9 @@ Util.xInObject = function(object) {
     </head>
     <body onLoad="bdetect()" style="margin: 0px;">
         <noscript>
-            <iframe frameborder="0" src="/main?wc=EWJnGhd5DB5fBBBzGAkFHQ%3D%3D&amp;url=localhostu-test.js" width="100%" height="100%"></iframe>
+            <iframe frameborder="0" src="/main?wc=EWJnGhd5DB5fBBBzGAkFHQ%3D%3D&amp;url=localhostu-relative.js" width="100%" height="100%"></iframe>
         </noscript>
     </body>
 </html>
+
+var absolute;
