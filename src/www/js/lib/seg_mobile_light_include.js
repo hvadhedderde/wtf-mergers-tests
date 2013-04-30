a7e348619751ae5458c3cfd84752120b6569f766
kaestel <martin@hvadhedderde.com>
2013-04-30 14:39:37 +0200

    test

---
+++ src/www/js/lib/seg_mobile_light_include.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="http://test.whattheframework.org/u.js"></script>');
+document.write('<script type="text/javascript" src="u-relative.js"></script>');
+document.write('<script type="text/javascript" src="/test/src/js/lib/u-absolute.js"></script>');
---
+++ src/www/js/lib/u-absolute.js
@@ -0,0 +1,2 @@
+//local
+var absolute;
