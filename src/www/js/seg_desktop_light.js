1a1e3e47f3f14ff69356af0bd7ce884de1e38d40
kaestel <kaestel@e-types.com>
2014-02-03 11:42:03 +0100

    added info

--- src/www/js/seg_desktop_light.js
+++ src/www/js/seg_desktop_light.js
@@ -1,23 +1,2 @@
 
 /*seg_desktop_light_include.js*/
-
-/*u.js*/
-if(!u || !Util) {
-	var u, Util = u = new function() {}
-	u.version = 0.7;
-	u.bug = function() {}
-	u.stats = new function() {this.pageView = function(){};this.event = function(){};this.customVar = function(){}}
-}
-
-
-/*u-relative.js*/
-
-/*u-absolute.js*/
-var absolute; 
-var after_comment;
-var relative;
-
-
-/*u-absolute.js*/
-var absolute; 
-var after_comment;
